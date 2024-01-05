0a40de7943ad5516e290ebc6dc64a32d8135f775
SANTHOSH03S <150016638+SANTHOSH03S@users.noreply.github.com>
2024-01-05 21:42:39 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,16 @@
+
+
+
+function show_list(){
+    document.getElementById('nav_list').style.display="block"
+    document.getElementById('show_list').style.display="none"
+    document.getElementById('close_list').style.display="block"
+    
+}
+
+function close_list(){
+    document.getElementById('nav_list').style.display="none"
+    document.getElementById('show_list').style.display="block"
+    document.getElementById('close_list').style.display="none"
+
+}
